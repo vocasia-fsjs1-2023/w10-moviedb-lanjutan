b0fe58ea4108c6bceb9bcf6530581e2c8a34ec33
saidatunnizmah <aniskateha@gmail.com>
2023-11-10 04:32:43 +0800

    tugas week 10

---
+++ assets/seeders/20231102320720-seeduser.js
@@ -0,0 +1,38 @@
+'use strict';
+const bcrypt = require("bcrypt");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   const hashpass = await bcrypt.hash("12345678",10);
+    await queryInterface.bulkInsert('Users', 
+    [
+      {
+        name: "nengmutiarahma",
+        email: "mutiarahma042@gmail.com",
+        password: hashpass,
+        isAdmin: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
